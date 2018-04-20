f1a6cdba1ba98d6ee0770d0fe446500e10447f03
Brandon <31549535+Bgreen23@users.noreply.github.com>
2018-04-20 14:21:54 -0400

    added Ch 6 and ch 7

---
+++ programming-phoenix/rumbl/web/models/video.ex
@@ -0,0 +1,24 @@
+defmodule Rumbl.Video do
+  use Rumbl.Web, :model
+
+  schema "videos" do
+    field :url, :string
+    field :title, :string
+    field :description, :string
+    belongs_to :user, Rumbl.User
+    belongs_to :category, Rumbl.Category
+
+    timestamps()
+  end
+  # @required_fields ~w(url title description)
+  # @optional_fields ~w()
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, [:url, :title, :description, :category_id])
+    |> assoc_constraint(:category)
+  end
+end
